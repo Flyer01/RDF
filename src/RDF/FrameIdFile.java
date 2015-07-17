ef367c553892b8b4cd501fd38159dbad723aadc8
Flyer <flyer@example.ru>
2015-07-17 08:40:07 +0400

    Описан новый frame

--- src/RDF/FrameIdFile.java
+++ src/RDF/FrameIdFile.java
@@ -1,10 +1,24 @@
 package RDF;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by 6377-00-285 on 16.07.2015.
  */
 public class FrameIdFile {
     public void frameIdFile() {
-        
+        JFrame frameOpenDialog = new JFrame();
+
+        frameOpenDialog.setSize(500, 500);
+        frameOpenDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frameOpenDialog.setLocationRelativeTo(null);
+        frameOpenDialog.setLayout(new GridBagLayout());
+
+        JFileChooser fileChooser1 = new JFileChooser();
+
+        frameOpenDialog.add(fileChooser1);
+
+        frameOpenDialog.setVisible(true);
     }
 }
